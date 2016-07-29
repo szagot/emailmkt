99eed974b99f0ba1e7d531a96a81604f2dfc23c7
= <=>
2016-07-29 14:08:35 -0300

    BUG: Correção de rotas deviso a troca de namespace

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -4,11 +4,11 @@ return [
     'dependencies' => [
         'invokables' => [
             Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\AuraRouter::class,
-            App\Action\PingAction::class => App\Action\PingAction::class,
+            EmailMKT\Action\PingAction::class => EmailMKT\Action\PingAction::class,
         ],
         'factories' => [
-            App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
-            App\Action\TestePageAction::class => App\Action\TestePageFactory::class,
+            EmailMKT\Action\HomePageAction::class => EmailMKT\Action\HomePageFactory::class,
+            EmailMKT\Action\TestePageAction::class => EmailMKT\Action\TestePageFactory::class,
         ],
     ],
 
@@ -16,19 +16,19 @@ return [
         [
             'name' => 'home',
             'path' => '/',
-            'middleware' => App\Action\HomePageAction::class,
+            'middleware' => EmailMKT\Action\HomePageAction::class,
             'allowed_methods' => ['GET'],
         ],
         [
             'name' => 'api.ping',
             'path' => '/api/ping',
-            'middleware' => App\Action\PingAction::class,
+            'middleware' => EmailMKT\Action\PingAction::class,
             'allowed_methods' => ['GET'],
         ],
         [
             'name' => 'teste',
             'path' => '/teste',
-            'middleware' => App\Action\TestePageAction::class,
+            'middleware' => EmailMKT\Action\TestePageAction::class,
             'allowed_methods' => ['GET'],
         ],
     ],
