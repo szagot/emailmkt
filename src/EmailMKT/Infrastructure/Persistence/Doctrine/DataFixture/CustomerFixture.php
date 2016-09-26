2523c43c3331ef29f267fd0d6cecbfc90f3cf057
Daniel Bispo <daniel@tmw.com.br>
2016-09-26 10:38:03 -0300

    Fixtures: Adicionado Faker na Fixture de Contato

--- src/EmailMKT/Infrastructure/Persistence/Doctrine/DataFixture/CustomerFixture.php
+++ src/EmailMKT/Infrastructure/Persistence/Doctrine/DataFixture/CustomerFixture.php
@@ -1,8 +1,15 @@
 <?php
 /**
  * Fixture: recurso de teste de desenvolvimento
+ *
  * Para transferir, execute no terminal:
  *      vendor/bin/doctrine-module data-fixture:import
+ *
+ * Para apenas adicionar dados, sem limpar o BD:
+ *      vendor/bin/doctrine-module data-fixture:import --append
+ *
+ * Para zerar o banco e também o auto-incremento, basta executar:
+ *      vendor/bin/doctrine-module data-fixture:import --purge-with-truncate
  */
 
 namespace EmailMKT\Infrastructure\Persistence\Doctrine\DataFixture;
@@ -10,17 +17,27 @@ namespace EmailMKT\Infrastructure\Persistence\Doctrine\DataFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use EmailMKT\Domain\Entity\Customer;
+use Faker\Factory as Faker;
 
 class CustomerFixture implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // Dado fake
-        $customer = new Customer();
-        $customer->setName('Daniel Bispo')->setEmail('daniel@tmw.com.br');
+        // Inicializando Faker
+        $faker = Faker::create();
+
+        // Criando 100 registros falsos
+        foreach (range(1, 100) as $value) {
+            // Dado fake
+            $customer = new Customer();
+            $customer
+                ->setName($faker->firstName . ' ' . $faker->lastName)
+                ->setEmail($faker->email);
+
+            $manager->persist($customer);
+        }
 
         // Registrando no BD
-        $manager->persist($customer);
         $manager->flush();
     }
 }
