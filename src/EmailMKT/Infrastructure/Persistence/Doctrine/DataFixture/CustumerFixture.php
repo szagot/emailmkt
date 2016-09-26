6a12c98891f37e14fedb8ef56096486e7ddaa405
Daniel Bispo <daniel@tmw.com.br>
2016-09-26 10:14:44 -0300

    Fixtures: Criado primeiro fixture de teste, e movido bibliotecas de fixtures e fake para "require-dev" no composer

---
+++ src/EmailMKT/Infrastructure/Persistence/Doctrine/DataFixture/CustumerFixture.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Fixture: recurso de teste de desenvolvimento
+ * Para transferir, execute no terminal:
+ *      vendor/bin/doctrine-module data-fixture:import
+ */
+
+namespace EmailMKT\Infrastructure\Persistence\Doctrine\DataFixture;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use EmailMKT\Domain\Entity\Customer;
+
+class CustumerFixture implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        // Dado fake
+        $customer = new Customer();
+        $customer->setName('Daniel Bispo')->setEmail('daniel.fixtures@tmw.com.br');
+
+        // Registrando no BD
+        $manager->persist($customer);
+        $manager->flush();
+    }
+}
