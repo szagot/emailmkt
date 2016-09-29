0523e40d3f6056a1cea99184d2dcb381ba1698c3
Daniel Bispo <szagot@gmail.com>
2016-09-29 10:46:23 -0300

    Login: Criada lógica de verificação de senha

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -1,6 +1,8 @@
 <?php
 // Configurações de Banco de dados para execução local
 
+use EmailMKT\Domain\Entity\User;
+
 return [
     'doctrine' => [
         'connection'     => [
@@ -40,6 +42,11 @@ return [
                 'identity_property'   => 'email',
                 // Campo de Senha
                 'credential_property' => 'password',
+                // Como autenticar a senha?
+                'credential_callable' => function (User $user, $passwordGiven) {
+                    // Verifica a senha passada comparando com a senha da entidade
+                    return password_verify($passwordGiven, $user->getPassword());
+                },
             ],
         ],
     ],
--- src/EmailMKT/Domain/Entity/User.php
+++ src/EmailMKT/Domain/Entity/User.php
@@ -12,6 +12,9 @@ class User
     private $email;
     private $password;
 
+    /** @var string Senha sem criptografar - não será persistido no BD */
+    private $plainPassword;
+
     /**
      * @return mixed
      */
@@ -78,6 +81,40 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string $plainPassword
+     *
+     * @return User
+     */
+    public function setPlainPassword(string $plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
+
+    /**
+     * Gera a senha, nova (aleatória) ou apenas criptografa
+     * Esse método deve ser chamado via callback antes de se persistir os dados no  BD
+     */
+    public function generatePassword()
+    {
+        // Pega a senha digitada ou gera uma senha aleatória
+        $password = $this->getPlainPassword() ? $this->getPlainPassword() : uniqid();
+
+        // Gera a senha criptografado
+        $this->setPassword(password_hash($password, PASSWORD_BCRYPT));
     }
 
 }
